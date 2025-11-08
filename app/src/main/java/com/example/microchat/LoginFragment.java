9e2c3290ee4c081a8c1d3edcbed9538f19cf1a8d
Aliveman <henegger@163.com>
2025-11-08 17:55:16 +0800

    新增登录历史菜单

--- app/src/main/java/com/example/microchat/LoginFragment.java
+++ app/src/main/java/com/example/microchat/LoginFragment.java
@@ -2,6 +2,11 @@ package com.example.microchat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +27,12 @@ public class LoginFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // 新增成员变量2025年11月8日00:06:44
+    private ConstraintLayout layoutContext;// 正常内容部分，是一个ConstraintLayout
+    private LinearLayout layoutHistory;// 历史菜单部分，是一个LinearLayout
+    private EditText editTextQQNum;// 输入框
+
 
     public LoginFragment() {
         // Required empty public constructor
@@ -57,13 +68,61 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        // 初始化成员变量
+        layoutContext = v.findViewById(R.id.layoutContext);
+        layoutHistory = v.findViewById(R.id.layoutHistory);
+        editTextQQNum = v.findViewById(R.id.editTextQQNum);
+
+        // 注册点击事件
         v.findViewById(R.id.textViewRegister).setOnClickListener(view -> {
-            //启动注册Activity
+            // 启动注册Activity
             Intent intent = new Intent(getContext(),RegisterActivity.class);
             startActivity(intent);
         });
-        // Inflate the layout for this fragment
+
+        // 响应下拉箭头的点击事件，弹出登录历史记录菜单
+        v.findViewById(R.id.textViewHistory).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutContext.setVisibility(View.INVISIBLE);
+                layoutHistory.setVisibility(View.VISIBLE);
+
+                // 创建两条历史记录菜单项，添加到layoutHistory中
+                for(int i=0;i<3;i++) {
+                    View layoutItem = getActivity().getLayoutInflater().inflate(R.layout.login_history_item, null);
+                    //响应菜单项的点击，把它里面的信息填到输入框中。
+                    layoutItem.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            editTextQQNum.setText("1234567890");
+                            layoutContext.setVisibility(View.VISIBLE);
+                            layoutHistory.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    layoutHistory.addView(layoutItem);
+                }
+
+                // 使用动画显示历史记录
+                AnimationSet set = (AnimationSet) AnimationUtils.loadAnimation(
+                        getContext(), R.anim.login_history_anim);
+                layoutHistory.startAnimation(set);
+            }
+        });
+
+        // 当点击菜单项之外的区域时，把历史菜单隐藏
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(layoutHistory.getVisibility()==View.VISIBLE){
+                    layoutContext.setVisibility(View.VISIBLE);
+                    layoutHistory.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
         return v;
     }
 }
